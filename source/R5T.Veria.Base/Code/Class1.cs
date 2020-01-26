34cd041bfb353c4bb486a2525f767aadabf342e7
MinexAutomation <minex.automation@gmail.com>
2020-01-26 01:36:21 -0800

    Added IRoundTripFileSerializationVerifier service definition interface type.

--- source/R5T.Veria.Base/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Veria.Base
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Veria.Base/Code/Services/Definitions/IRoundTripFileSerializationVerifier.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace R5T.Veria
+{
+    /// <summary>
+    /// Allows verification that an object is the same after round-trip serialization then deserialization.
+    /// </summary>
+    public interface IRoundTripFileSerializationVerifier<T>
+    {
+        /// <summary>
+        /// Performs serialization, deserialization, value-comparison, re-serialization, and file-comparison to verify that an object and file is the same after de/serialization.
+        /// </summary>
+        bool Verify(T value, string serializationFilePath1, string serializationFilePath2);
+        /// <summary>
+        /// Performs deserialization, serialization, file-comparison, re-deserialization, and value-comparison to verify that an object and file is the same after de/serialization.
+        /// </summary>
+        bool Verify(string sourceFilePath, string serializationFilePath);
+    }
+}
