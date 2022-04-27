0b6eef5964357a875a5e339acf0d7c54dbf49eee
davidcoats <david@getsafetycone.com>
2022-04-27 15:37:28 -0700

    Interim changes.

--- source/R5T.Veria.Base/Code/Services/Definitions/IRoundTripFileSerializationVerifier.cs
+++ source/R5T.Veria.Base/Code/Services/Definitions/IRoundTripFileSerializationVerifier.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 
+using R5T.T0064;
+
 
 namespace R5T.Veria
 {
     /// <summary>
     /// Allows verification that an object is the same after round-trip serialization then deserialization.
     /// </summary>
-    public interface IRoundTripFileSerializationVerifier<T>
+    [ServiceDefinitionMarker]
+    public interface IRoundTripFileSerializationVerifier<T> : IServiceDefinition
     {
         /// <summary>
         /// Performs serialization, deserialization, value-comparison, re-serialization, and file-comparison to verify that an object and file is the same after de/serialization.
